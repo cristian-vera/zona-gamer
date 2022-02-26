da98f8cb6b60427918c1698dc42589ad8ded3a58
cristian-vera <cristian.axel.vera@gmail.com>
2022-02-26 00:55:57 -0300

    modificacion de primera version

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function NavBar(){
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="##"> Zona gamer</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="##">INICIO</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="##">PS/XBOX</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="##">PC</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="##">CONTACTO</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
